e5ab838a68bcdefb844235c524da95362e5f1617
Isaac - Exon <isaac.souza@exongenomics.com>
2025-04-17 21:17:34 -0300

    fix: Update API baseURL from localhost to the local network IP address

--- front/services/api.ts
+++ front/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { loadFromLocalStorage } from "../utils/localStorage";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://192.168.108.195:3000",
   headers: {
     "Content-Type": "application/json",
   },
